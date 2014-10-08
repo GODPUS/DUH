93c1b2cea7b80d37e76eaf2690f1584dbb99f10e
GODPUS <mikeparisi@gmail.com>
2014-10-08 11:36:35 -0700

    fixed scrollspy

--- js/demo.js
+++ js/demo.js
@@ -1,11 +1,11 @@
 $(function(){
-	$('.demo-tab-pane').on('DUH.show', function(){
+	$('.demo-tab-pane').on('DUH:show', function(){
 		var $this = $(this);
 		var curHeight = $this.height();
 		$this.css('height', 'auto');
 		var autoHeight = $this.height();
 		$this.height(curHeight).stop().animate({ height: autoHeight }, 150);
-	}).on('DUH.hide', function(){
+	}).on('DUH:hide', function(){
 		$(this).stop().animate({ height: 0 }, 150);
 	});
 });
