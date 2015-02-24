d974d737c7d32b6969daf827fd1043a7656b8cdc
Michael Parisi <mparisi@organic.com>
2015-02-24 12:02:59 -0800

    adding jquery animate helper methods and global jquery objects of document, window, html, body

--- demo/demo.js
+++ demo/demo.js
@@ -1,21 +1,18 @@
 $(function(){
 
-	//use custom jquery onlyOn method to prevent parent elements with same selector from running callback function while still maintaining 'live' functionality.
-	$('body').onlyOn(DUH.events.activate, '.demo-tab-pane', function(event){
-		var $this = $(this);
-		var curHeight = $this.height();
-		$this.css('height', 'auto');
-		var autoHeight = $this.height();
-		$this.height(curHeight).stop().animate({ height: autoHeight }, 150, function(){ $this.css('height', 'auto'); });
+	//use custom jquery onlyOn method to prevent parent elements with same 
+	//selector from running callback function while still maintaining 'live' functionality.
+	$body.onlyOn(DUH.events.activate, '.demo-tab-pane', function(event){
+
+		//use convienent animateAutoHeight method that takes the parameters (animationOptions, callback)
+		$(this).animateAutoHeight({ speed: 150 }, function(){ console.log('auto height animation complete!') });
+
 	}).onlyOn(DUH.events.deactivate, '.demo-tab-pane', function(event){
-		$(this).stop().animate({ height: 0 }, 150);
+
+		//use convienent animateZeroHeight method that takes the parameters (animationOptions, callback)
+		$(this).animateZeroHeight({ speed: 150 }, function(){ console.log('zero height animation complete!') });
+
 	});
-
-	$('<div id="tab-pane-4" data-group="tab-panes-group-1" class="demo-tab-pane">TAB PANE 4 !</div>').insertAfter('[data-toggle="#tab-pane-4"]');
-
-	DUH.activate($('#tab-pane-2'));
-	DUH.deactivate($('#tab-pane-2'));
-	DUH.toggle($('#tab-pane-2'));
 
 	DUH.onBreakpoint(function(){
 		console.log('ALL!');
